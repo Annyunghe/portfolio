8680ff3c25e0f2724eeb9fbd523ea548b8f3faf9
bulkkoch <137600477+bulkkoch@users.noreply.github.com>
2024-10-16 00:12:44 +0900

    first commit
    레시피 프로젝트 초기 버전

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+export default function Header() {
+  const barStyle =
+    "w-auto text-sm text-gray-100 p-2 font-bold hover:text-amber-900";
+
+  return (
+    <div className="mt-2 flex flex-row justify-center bg-amber-600 gap-10">
+      <button className={barStyle}>레시피 모음</button>
+      <button className={barStyle}>최근 조회</button>
+      <button className={barStyle}>순위</button>
+      <button className={barStyle}>검색</button>
+    </div>
+  );
+}
